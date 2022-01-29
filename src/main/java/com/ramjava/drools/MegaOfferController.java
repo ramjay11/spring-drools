39fba07aadda6b72089ab7268dd7b8b775b96bbb
Romorosa <ramjay.romorosa@accenture.com>
2022-01-30 02:06:30 +0800

    rule engine fixes

--- src/main/java/com/ramjava/drools/MegaOfferController.java
+++ src/main/java/com/ramjava/drools/MegaOfferController.java
@@ -1,5 +1,7 @@
 package com.ramjava.drools;
 
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MegaOfferController {
 
+	@Autowired
+	private KieSession session;
+	
 	@PostMapping("/order")
-	public String orderNow(@RequestBody Order order) {
+	public Order orderNow(@RequestBody Order order) {
 		// Rule engine
-		return null;
-		
+		session.insert(order);
+		session.fireAllRules();
+		return order;
 	}
 }
